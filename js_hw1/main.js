7a933afe1fee6a7e44cd3ff2835cdc72ac3653d8
sfrolov008 <sfrolov008@gmail.com>
2022-11-01 18:23:29 +0200

    js_hw1

---
+++ js_hw1/main.js
@@ -0,0 +1,72 @@
+let v1 = 'hello';
+console.log(v1);
+
+let v2 = 'owu';
+console.log(v2);
+
+let v3 = 'com';
+console.log(v3);
+
+let v4 = 'ua';
+console.log(v4);
+
+let num1 = 1;
+console.log(num1);
+
+let num2 = 10;
+console.log(num2);
+
+let num3 = -999;
+console.log(num3);
+
+let num4 = 123;
+console.log(num4);
+
+const PI = 3.14;
+console.log(PI);
+
+const E = 2.71;
+console.log(E);
+
+let num5 = 16;
+console.log(num5);
+
+let boolT = 5 === 5;
+console.log(boolT);
+
+let boolF = 5 != 5;
+console.log(boolF);
+
+
+let firstName = 'Serhii';
+let lastName = 'Frolov';
+let middleName = 'by_Alex';
+let person = (`${firstName} ${lastName} ${middleName}`);
+console.log(person);
+
+let a = 100;
+let b = 200;
+let c = 100 < 200;
+console.log(typeof a, typeof b, typeof c);
+
+let fName = prompt('first_name', '');
+let lName = prompt('last_name', '');
+let age = prompt('age', '');
+let customer = (`Wellcome  ${fName} ${lName} ${age}`);
+console.log(customer);
+
+// joke ))
+// true or fasle
+
+let letsDrink = true;
+if (letsDrink) {
+    console.log(':)');
+} else {
+    console.log(':(');
+}
+
+
+
+
+
+
